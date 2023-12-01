9c923e9f34bc20c479b6fb6841118995bd88f4f0
EdvinN1 <nedde14@gmail.com>
2023-12-01 15:15:16 +0100

    first commit

---
+++ src/main/java/com/example/projectdropbox/ProjectDropboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectdropbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDropboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDropboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectdropbox/ProjectDropboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectdropbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDropboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
